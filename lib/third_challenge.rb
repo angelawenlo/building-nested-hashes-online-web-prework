7fa4171d9c97ba0f338873ac2e9952a9e4b7d997
Angela Lo <angelawenlo@gmail.com>
2019-05-17 14:41:50 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,4 +1,21 @@
 def third_challenge
+    epic_tragedy = {
+      :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+      },
+      :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+      }
+    }
+
+
+  end
 
 
 

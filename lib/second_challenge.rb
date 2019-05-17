bf04315ae40ca5b4761330a7f142831c82cfc17e
Angela Lo <angelawenlo@gmail.com>
2019-05-17 14:39:12 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,4 +1,13 @@
 def second_challenge
+  epic_tragedy = {
+    :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+    },
+    :capulet => {}
+  }
 
 
 end

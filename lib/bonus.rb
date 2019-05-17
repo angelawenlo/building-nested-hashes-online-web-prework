56d0c43776780533b7455aa2583d191fc869ddbb
Angela Lo <angelawenlo@gmail.com>
2019-05-17 14:49:49 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,6 +1,4 @@
 def bonus
-
-  #code your solution here:
   epic_tragedy = {
     :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
@@ -21,6 +19,8 @@ def bonus
       ]
     }
   }
+
+  #code your solution here:
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
